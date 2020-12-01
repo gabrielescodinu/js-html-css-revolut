6341b13d17483f7b49420b88534fed9f7df0cb4a
gabriele <gabrielescodinu96@gmail.com>
2020-12-01 10:52:15 +0100

    commit 03

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-$(".navbar-item").click(function(){
+$(".navbar-item").hover(function(){
+  // nascondo tutte le liste menu ad ogni hover, così che
+  // se faccio hover su un nuovo elemento il menu di quello prima scompare.
   $(".list").hide();
+  // vado a selezionare l'elemento dentro i navbar-item con .children
+  // e con .show lo mostro.
   $(this).children().show();
-  console.log($(this).children());
+})
+
+// questa funzione invece serve per togliere il menu
+// quando si esce con l'hover.
+$(".navbar-item").mouseleave(function(){
+  $(".list").hide();
 })
